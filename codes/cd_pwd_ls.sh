86ce35af315b68d8ef7f6b9ed8d61492c9bc4215
costRider <107491279+costRider@users.noreply.github.com>
2025-07-14 18:12:42 +0900

    study commit

--- codes/cd_pwd_ls.sh
+++ codes/cd_pwd_ls.sh
@@ -1,5 +1,8 @@
 # cd_pwd_ls.sh 2025.07.14 - 이민규
 # 수업과제 공유용
+~$ pwd # 현재 위치 확인용
+[user01@localhost ~]$ pwd
+/home/user01
 [user01@localhost home]$ cd /home/user01/Music/ # /home/user01/Music/ 디렉토리 이동
 [user01@localhost Music]$ ls # 내용 보기
 [user01@localhost Music]$ cd /home/user01/Pictures/ #/home/user01/Pictures/ 디렉토리 이동
