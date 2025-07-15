e0e94ae5682e5ff56d473cfe48e350413dfa355c
costRider <107491279+costRider@users.noreply.github.com>
2025-07-15 12:23:03 +0900

    readme file update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # study_linuxs
 BespinGlobal x Green Computer Acamedy Study
+
+-------------
+
+
+### codes 폴더 
+* [실습 및 과제](https://github.com/costRider/study_linuxs/blob/51027f6cb0f16dbf1c8b18eebce692cacdbb7015/codes)
+
+### study 폴더 
+* [수업 실습 정리](https://github.com/costRider/study_linuxs/blob/51027f6cb0f16dbf1c8b18eebce692cacdbb7015/study)
+
--- study/linux_20250715.sh
+++ study/linux_20250715.sh
@@ -1,5 +1,38 @@
-#2025.07.15 linux_20250715.sh - 이민규
-# 수업 과제 정리
+####################################
+#   2025.07.15 linux_20250715.sh - 이민규
+#   수업 실습 정리
+#   linux 명령어를 powershell 에서 연습#
 #
+#   cd - changeDirectory
+#   pwd - printworkingdirectory
+#   ls - list
+#######################################
+
+cd C:\
+cd C:\Develops\
+pwd
+ls
+
+###################### 결과 값
+
+PS C:\Develops> cd C:\
+PS C:\> cd C:\Develops\
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:58                Downloads
+d-----      2025-07-15   오전 9:02                VMs
 
 
