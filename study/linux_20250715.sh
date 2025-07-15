64768e164ea5e4518857a7496a09e5eb5fab6ac0
costRider <107491279+costRider@users.noreply.github.com>
2025-07-15 14:32:53 +0900

    study lab

--- study/linux_20250715.sh
+++ study/linux_20250715.sh
@@ -35,4 +35,53 @@ Mode                 LastWriteTime         Length Name
 d-----      2025-07-14  오전 11:58                Downloads
 d-----      2025-07-15   오전 9:02                VMs
 
+#####################
+## 파일, 폴더 생성 삭제 
+###################
+#
+#   mkdir - 폴더 생성
+#   
+########################
+
+
+PS C:\Develops> mkdir temp_dir
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:26                temp_dir
+
+################################################
+#   new-item 파일 생성
+################################################
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:58                Downloads
+d-----      2025-07-15   오후 2:26                temp_dir
+d-----      2025-07-15   오전 9:02                VMs
+
+
+
+PS C:\Develops> cd .\temp_dir\
+PS C:\Develops\temp_dir> ls
+PS C:\Develops\temp_dir> new-item file_first.txt
+
+
+    디렉터리: C:\Develops\temp_dir
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a----      2025-07-15   오후 2:27              0 file_first.txt
+
+################################################################
 
